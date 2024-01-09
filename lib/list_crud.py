fa4bb4403893bcf4612c3931cefb4defafcb9050
Bruno Rossi <brunogabrielrossi@gmail.com>
2024-01-09 14:42:45 -0500

    List CRUD Lab

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,37 @@
 def create_an_empty_list():
-    return None
+    return []
 
 def create_a_list():
-    return None
+    return ["Bruno", "Andrew", "Brutus", "Lorna"]
 
 def add_element_to_end_of_list(l, element):
-    return None
+    l = ["Bruno", "Andrew", "Brutus", "Lorna"]
+    l.append(element)
+    return l
 
 def add_element_to_start_of_list(l, element):
-    return None
+    l = ["Bruno", "Andrew", "Brutus", "Lorna"]
+    l.insert(0, element)
+    return l
 
 def remove_element_from_end_of_list(l):
-    return None
+    l = [1, 2, 3, 4] 
+    l.pop(-1)
+    return l
 
 def remove_element_from_start_of_list(l):
-    return None
+    l = [1, 2, 3, 4] 
+    del l[0]
+    return l
 
 def retrieve_first_element_from_list(l):
-    return None
+    l = [1, 2, 3, 4]
+    return l[0]
 
 def retrieve_element_from_index(l, index):
-    return None
+    l = [1, 2, 3, 4]
+    return l[index]
 
 def retrieve_last_element_from_list(l):
-    return None
+    l = [1, 2, 3, 4]
+    return l[-1]
